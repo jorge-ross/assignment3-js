e491f73360b253595e12574cabf6c061a90e2e10
Jorge Ross <jorg.ross.13@gmail.com>
2023-06-28 10:36:59 -0600

    add: solution to 19regular-ball.js

---
+++ 19regular-ball.js
@@ -0,0 +1,11 @@
+// Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+// If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+class Ball {
+  constructor(ballType = "regular") {
+    this.ballType = ballType;
+  }
+}
+
+// let myBall = new Ball("super");
+// console.log(myBall.ballType);
